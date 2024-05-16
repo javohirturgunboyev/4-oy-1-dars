5ce0b4137b7fdbb9a4962cf232542ef7fa09046d
javohirturgunboyev <javohirboy009@gmail.com>
2024-05-16 22:11:56 +0500

    masalalar tugadi

---
+++ index.js
@@ -0,0 +1,131 @@
+// 1-misol
+// let L = 100
+// console.log(L/100)
+
+
+
+// 2-misol
+// let M = 1000
+// console.log(M/1000)
+
+
+
+// 3-misol
+// let kb = 1024
+// console.log(kb/1024)
+
+
+
+// 4-masala
+// let m =+ prompt("m ni kiriting");
+// let n =+ prompt("n ni kiriting");
+// let o = m/n ;
+// o =parseInt(o);
+// document.write(o);
+
+
+
+// 5-masala
+
+// 6-masala
+// let number =+ prompt("ikki xonali son kiriting");
+// let a = number/10;
+// let b = number%10;
+// a =preseInt(a);
+// b =preseInt(b);
+// document.write(a);
+// document.write("va");
+// document.write(b);
+
+
+
+// 7-masala
+// let number =+prompt("ikki xonali son kiriting");
+//let m = number%10; 
+// let n = number/10;
+// m =pareseInt(m);
+// n =pareseInt(n);
+// document.write(m+n);
+
+
+
+// 8-masala
+// let son =+prompt("ikki xonali sonni kiriting");
+// let m = son/10;
+// let n = son%10;
+// m =prareseInt(m);
+// n =pareseInt(n);
+// document.write(n);
+// document.write(m);
+// document.write("va");
+// document.write(m+n);
+
+// 9-masala
+// let son =+ prompt ("uch xonali son kiriting");
+// let m =son/100;
+// m = pareseInt(a);
+// document.write(a);
+
+
+// 10-masala
+// let son =+ prompt("uch xonali son kiriting");
+// let m = son/10;
+// let n = m/10;
+// let o = m%10;
+// let p = son%100;
+// let q = p%10;
+// let m = pareseInt(m);
+// let n = pareseInt(n);
+// let o = pareseInt(o);
+// let p = pareseInt(p);
+// let q = pareseInt(q);
+// let document.write(q);
+// let document.write(o);
+
+// 11-masala
+// let son =+ prompt("uch xonali son kiriting");
+// let m = son/10;
+// let n = m/10;
+// let o =m%10;
+// let p = son%100;
+// let q = p%10;
+// let m = preseInt(m);
+// let n = preseInt(n);
+// let o = preseInt(o);
+// let p = preseInt(p);
+// let q = preseInt(q);
+// document.write(n+o+q);
+
+
+// 12-masala
+// let son =+prompt("uch xonali son kiriting");
+// let m = son/10;
+// let n = m/10;
+// let o = m%10;
+// let p = son%100;
+// let q = p%10;
+// m = pareseInt(m);
+// n = pareseInt(n);
+// o = pareseInt(o);
+// p = pareseInt(p);
+// q = pareseInt(q);
+// document.write(q);
+// document.write(o);
+// document.write(n);
+
+
+// 13-masala
+// let son =+ prompt("uch xonali son kiriting");
+// let m = son/10;
+// let n = m/10;
+// let o = m%10;
+// let p = son%100;
+// let q = p%10;
+// m = pareseInt(m);
+// n = pareseInt(n);
+// o = pareseInt(o);
+// p = pareseInt(p);
+// q = pareseInt(q);
+// document.write(o);
+// document.write(q);
+// document.write(n);
